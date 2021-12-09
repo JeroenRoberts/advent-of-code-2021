9d8594fa19494358f5e20825a4da2e17cba06668
JeroenRoberts <jeroenrobertsss@gmail.com>
2021-12-09 07:52:01 +0100

    small_input correct

--- day_9/puzzle.py
+++ day_9/puzzle.py
@@ -1,42 +1,35 @@
 import numpy as np
 
-def minimum_single_row(heights):
-    first_row = heights[0, :]
-    first_row_shift_right = np.roll(heights[0, :], 1)
-    first_row_shift_right[0] = 99999 #invalid neighbour (no pbc)
-    first_row_shift_left = np.roll(heights[0, :], -1)
-    first_row_shift_left[-1] = 99999 #invalid neighbour (no pbc)
-    print(first_row)
-    print(first_row_shift_right)
-    print(first_row_shift_left)
-    minimum = np.minimum(first_row_shift_left, first_row_shift_right)
-    print(first_row < minimum)
+def get_local_minimum_matrix(matrix):
+    directions = [-1, 1]
+    axis = np.array([0, 1])
+    neighbours = {}
+    neighbours['up'] = {'direction': -1, 'axis': 0, 'type': 'row', 'index': -1}
+    neighbours['down'] = {'direction': 1, 'axis': 0, 'type': 'row', 'index': 0}
+    neighbours['left'] = {'direction': -1, 'axis': 1, 'type': 'column', 'index': -1}
+    neighbours['right'] = {'direction': 1, 'axis': 1, 'type': 'column', 'index': 0}
+    translations = {}
+    for n, n_data in neighbours.items():
+        roll = np.roll(matrix, n_data['direction'], axis=n_data['axis'])
+        if n_data['type'] == 'row':
+            roll[n_data['index'], :] = 99999
+        elif n_data['type'] == 'column':
+            roll[:, n_data['index']] = 99999
+        translations[n] = roll
+
+    local_minimum = 99999 * np.ones(matrix.shape, dtype=int)
+    for k, t in translations.items():
+        local_minimum = np.minimum(local_minimum, t)
+    return local_minimum
+
 
 if __name__ == "__main__":
     file = 'small_input.txt'
     heights = np.genfromtxt(file, delimiter=1, dtype=int)
-    min_of_neighbours = np.zeros(heights.shape)
-
-    shift_right = np.roll(heights, 1)
-    shift_right[:, 0] = 99999 #invalid neighbour (no pbc)
-
-
-    shift_left = np.roll(heights, -1)
-    shift_left[:, -1] = 99999 #invalid neighbour (no pbc)
-
-
-    shift_up = np.roll(heights, -1, axis)
-    shift_up[:, -1] = 99999 #invalid neighbour (no pbc)
-    print(heights)
-    print(shift_right)
-    print(shift_left)
-
-
-    
-
-    # print(heights.shape)
-    # for k in range(heights.shape[0]):
-    #     for l in range(heights.shape[1]):
-    #     print(k, l)
-
+    min = get_local_minimum_matrix(heights)
     # print(heights)
+    # print(min)
+    # print(heights < min)
+    risk = (heights+1)[heights < min].sum()
+    print(f'{risk = }')
+
