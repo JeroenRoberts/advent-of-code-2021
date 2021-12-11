a28ca327906e6360b8534dcd915bc0f00bd9bc5c
JeroenRoberts <jeroenrobertsss@gmail.com>
2021-12-11 08:57:21 +0100

    small input correct; had to fix negative indexes

--- day_11/puzzle_1.py
+++ day_11/puzzle_1.py
@@ -1,16 +1,8 @@
 import numpy as np
 
-def check_boundary(i: int, bound: int) -> bool:
-    if i >= 0 and i < bound:
-        return True
-    else:
-        return False
-
-
-# if check_boundary(i, data.shape[0]) and check_boundary(j, data.shape[1]):
 def increment_neighbours(data: np.array, i: int, j: int) -> None:
-    for k in range(i-1, i+2):
-        for l in range(j-1, j+2):
+    for k in range(max(i-1, 0), i+2):
+        for l in range(max(j-1, 0), j+2):
             if k == i and l == j: #self
                 pass
             else: #neighbour
@@ -22,9 +14,7 @@ def increment_neighbours(data: np.array, i: int, j: int) -> None:
 
 def perform_step(data: np.array):
     #1. Increment all
-    for i in range(data.shape[0]):
-        for j in range(data.shape[1]):
-            data[i, j] += 1
+    data += 1
 
     #2. flashing
     flashed = np.zeros(data.shape, dtype=bool)
@@ -34,6 +24,7 @@ def perform_step(data: np.array):
                 if data[i, j] > 9 and flashed[i, j] == False:
                     flashed[i, j] = True
                     increment_neighbours(data, i, j)
+
     #3. octopus that flashed gets set to 0
     data[flashed] = 0
 
@@ -48,33 +39,3 @@ if __name__ == "__main__":
         print(data)
         perform_step(data)
     print(data)
-    # data[0, 0] = 9
-    # for i in range(data.shape[0]):
-    #     for j in range(data.shape[1]):
-    #         data[i, j] += 1
-
-    # flashed = np.zeros(data.shape, dtype=bool)
-    # # print(np.where(data > 9))
-    # # print(flashed
-    # # print(flashed[np.where(data > 9)])
-    # # print(np.all(flashed[np.where(data > 9)]))
-    # while np.all(flashed[np.where(data > 9)]) == False :
-    #     # print(data)
-    #     # print(flashed[np.where(data > 9)])
-    #     # print(np.all(flashed[np.where(data > 9)]))
-    #     for i in range(data.shape[0]):
-    #         for j in range(data.shape[1]):
-    #             if data[i, j] > 9 and flashed[i, j] == False:
-    #                 flashed[i, j] = True
-    #                 increment_neighbours(data, i, j)
-    # # print(data)
-    # # print(flashed[np.where(data > 9)])
-    # # print(np.all(flashed[np.where(data > 9)]))
-
-    # data[flashed] = 0
-    # print(data)
-    # # for i in range(data.shape[0]):
-    # #     for j in range(data.shape[1]):
-
-
-
